b576a46c0cb44cc32e2ca5dcc8af98448b3cf8e1
Rachel Oh <ryoh@usc.edu>
2024-11-02 01:09:31 -0700

    log in done

--- app/src/main/java/com/example/treehole/MainActivity.java
+++ app/src/main/java/com/example/treehole/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.treehole;
 
+import static android.content.ContentValues.TAG;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     // database information
@@ -69,12 +79,99 @@ public class MainActivity extends AppCompatActivity {
 
     // signs the user in
     public void onSignInClick(View view) {
+        //retrieves sign in information
         userInput = findViewById(R.id.userInput);
         passInput = findViewById(R.id.passInput);
 
         String username = userInput.getText().toString();
         String password = passInput.getText().toString();
 
+        if(validateEmail(username)) {
+            String shortUser = username.substring(0, username.indexOf("@"));
+            DatabaseReference userRef = reference.child("users").child(shortUser);
 
+            ValueEventListener eventListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        HashMap<String, String> info = (HashMap<String, String>) dataSnapshot.getValue();
+                        TextView updateMsg = findViewById(R.id.updateMessage);
+                        // checks if password is correct
+                        if(Objects.equals(info.get("password"), password))
+                        {
+                            SignIn();
+                        }
+                        else
+                        {
+                            SignInError(1);
+                        }
+                    } else {
+                        SignInError(0);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.w(TAG, "loadPost:onCancelled", error.toException());
+                }
+            };
+            userRef.addListenerForSingleValueEvent(eventListener);
+        }
+    }
+
+    //ensures that inputted email is formatted correctly and a USC email
+    @SuppressLint("SetTextI18n")
+    private boolean validateEmail(String email) {
+        boolean validUser = true;
+        TextView updateMsg = findViewById(R.id.updateMessage);
+
+        if(email.isEmpty() || !email.matches("^[a-zA-Z0-9._%+-]+@usc\\.edu$"))
+        {
+            validUser = false;
+            updateMsg.setText("** Invalid Email");
+        }
+        else
+        {
+            updateMsg.setText("");
+        }
+
+        return validUser;
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void SignIn() {
+        TextView updateMsg = findViewById(R.id.updateMessage);
+        updateMsg.setText("Sign In Successful!");
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void SignInError(int type) {
+        TextView updateMsg = findViewById(R.id.updateMessage);
+        if(type == 0)
+            updateMsg.setText("** User does not exist, please sign up");
+        else if(type == 1)
+            updateMsg.setText("** Incorrect Password");
+    }
+
+    public void getPassword(String username) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users").child(username);
+
+        databaseReference.child("password").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Retrieve the password value as a String
+                    String password = dataSnapshot.getValue(String.class);
+                    System.out.println("Password: " + password);
+                } else {
+                    System.out.println("User not found or password field does not exist.");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("Error: " + databaseError.getMessage());
+            }
+        });
     }
 }
